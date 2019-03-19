61dead11a01b84613252beef45725c698550aae7
AndySong <andylauszp@gmail.com>
2019-03-19 22:58:27 +1300

    wip

--- lib/src/screens/home_page.dart
+++ lib/src/screens/home_page.dart
@@ -1,36 +1,57 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../mocks/email_mock.dart';
 import '../models/email.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
 
+  HomePage({this.title});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<Email> emails = EmailMock.fetchAll();
 
-  HomePage({this.title});
+  var messages = [];
+
+  Future loadEmails() async {
+    var content = await rootBundle.loadString('data/message.json');
+    messages = json.decode(content);
+  }
+
+  @override
+  void initState() {
+    loadEmails();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: ListView.separated(
-          itemCount: emails.length,
+          itemCount: messages.length,
           separatorBuilder: (BuildContext context, int index) {
             return Divider();
           },
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              title: Text('${emails[index].title}'),
+              title: Text('${messages[index]['title']}'),
               leading: CircleAvatar(
                 child: Text('AS'),
               ),
               isThreeLine: true,
               subtitle: Text(
-                '${emails[index].message}',
+                '${messages[index]['message']}',
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
               ),
