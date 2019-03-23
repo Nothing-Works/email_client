171e12b6059f31b5b7eef6a62ec348f04fa10d95
AndySong <andylauszp@gmail.com>
2019-03-23 20:59:59 +1300

    add send message back page

---
+++ lib/src/screens/send_message_back.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SendMessageBack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New one'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Compose new message',
+                style: Theme.of(context).textTheme.title),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'Love');
+                  },
+                  child: Text('Love'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'Hate');
+                  },
+                  child: Text('Hate'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
