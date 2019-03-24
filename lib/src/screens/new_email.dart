f40a32f427f1cc6c3bd50c9c4fa5722370d747cb
AndySong <andylauszp@gmail.com>
2019-03-24 21:01:08 +1300

    change to form widget

--- lib/src/screens/new_email.dart
+++ lib/src/screens/new_email.dart
@@ -11,6 +11,8 @@ class _NewEmailState extends State<NewEmail> {
   String subject;
   String body;
 
+  final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,49 +20,54 @@ class _NewEmailState extends State<NewEmail> {
         title: Text('New one'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  to = value;
-                },
-                decoration: InputDecoration(
-                    labelText: 'To',
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+        child: Form(
+          key: key,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: TextFormField(
+                  onSaved: (value) {
+                    to = value;
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'To',
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ),
               ),
-            ),
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  subject = value;
-                },
-                decoration: InputDecoration(
-                    labelText: 'Subject',
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+              ListTile(
+                title: TextFormField(
+                  onSaved: (value) {
+                    subject = value;
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'Subject',
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ),
               ),
-            ),
-            Divider(),
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  body = value;
-                },
-                maxLines: 9,
-                decoration: InputDecoration(
-                    labelText: 'Body',
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+              Divider(),
+              ListTile(
+                title: TextFormField(
+                  onSaved: (value) {
+                    body = value;
+                  },
+                  maxLines: 9,
+                  decoration: InputDecoration(
+                      labelText: 'Body',
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ),
               ),
-            ),
-            ListTile(
-              title: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, Email(message: body, title: subject));
-                },
-                child: Text('Send'),
-              ),
-            )
-          ],
+              ListTile(
+                title: RaisedButton(
+                  onPressed: () {
+                    key.currentState.save();
+                    Navigator.pop(
+                        context, Email(message: body, title: subject));
+                  },
+                  child: Text('Send'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
