57b9e4aa9a238b9082ec958c28f34bf303b342ca
AndySong <andylauszp@gmail.com>
2019-04-27 10:43:24 +1200

    some format

--- lib/src/screens/send_message_back.dart
+++ lib/src/screens/send_message_back.dart
@@ -4,36 +4,30 @@ class SendMessageBack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('New one'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Compose new message',
-                style: Theme.of(context).textTheme.title),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () {
-                    Navigator.pop(context, 'Love');
-                  },
-                  child: Text('Love'),
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    Navigator.pop(context, 'Hate');
-                  },
-                  child: Text('Hate'),
-                ),
-              ],
-            )
-          ],
+        appBar: AppBar(
+          title: Text('New one'),
         ),
-      ),
-    );
+        body: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Compose new message',
+                      style: Theme.of(context).textTheme.title),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        RaisedButton(
+                            onPressed: () {
+                              Navigator.pop(context, 'Love');
+                            },
+                            child: Text('Love')),
+                        RaisedButton(
+                            onPressed: () {
+                              Navigator.pop(context, 'Hate');
+                            },
+                            child: Text('Hate'))
+                      ])
+                ])));
   }
 }
