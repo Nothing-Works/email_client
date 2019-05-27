30a5dd29caae9de723ea953fc54d12e09a6c8291
AndySong <andylauszp@gmail.com>
2019-05-27 21:43:36 +1200

    add observable

--- lib/src/blocs/contact_bloc/contact_bloc.dart
+++ lib/src/blocs/contact_bloc/contact_bloc.dart
@@ -1,17 +1,15 @@
-import 'dart:async';
-
 import 'package:rxdart/rxdart.dart';
 
 import '../../models/contact.dart';
 import '../../service/contact_service.dart';
 
 class ContactManager {
-  Stream<List<Contact>> get fetchContactList => _collectionSubject.stream;
+  Observable<List<Contact>> get fetchContactList => _collectionSubject.stream;
   Sink<String> get inFilter => _filterSubject.sink;
-  Stream<int> get contactCounter => _counterSubject.stream;
+  Observable<int> get contactCounter => _counterSubject.stream;
 
   ContactManager() {
-    _filterSubject.stream.listen((filter) async {
+    _filterSubject.listen((filter) async {
       var contacts = await ContactService.fetchAll(query: filter);
       _collectionSubject.add(contacts);
     });
@@ -21,7 +19,7 @@ class ContactManager {
   final PublishSubject<List<Contact>> _collectionSubject =
       PublishSubject<List<Contact>>();
   final PublishSubject<String> _filterSubject = PublishSubject<String>();
-  final BehaviorSubject<int> _counterSubject = BehaviorSubject<int>();
+  final PublishSubject<int> _counterSubject = PublishSubject<int>();
 
   void dispose() {
     _collectionSubject.close();
